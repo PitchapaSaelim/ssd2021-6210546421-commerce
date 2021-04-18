52fb8395d4bae819201afaccc725997a493b44c5
PitchapaSaelim <pitchapa.sae@ku.th>
2021-04-19 00:33:39 +0700

    Homework 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,19 @@ class HomeController < ApplicationController
   #     .search(@search) if @search.present?
   #   @products = @products.page(params[:page]).per(9)
   # end
+      # GET /products or /products.json
+    def index
+      @categories = Category.all
+      @products = Product.all
+      @search = params[:search]
+
+      @products = @products
+        .search(@search) if @search.present?
+      @products = @products.page(params[:page]).per(6)
+
+      respond_to do |format|
+        format.html
+        format.csv{ send_data generate_csv(Product.all), file_name: 'products.csv' }
+      end
+    end
 end
